adbd60fd8ab04908bbde55b20d4fa2a961eb7356
mimrock <lados.denes@gmail.com>
2015-01-24 19:20:28 +0100

    First touches of session handling and rendering

--- main.go
+++ main.go
@@ -5,17 +5,35 @@ import (
 	"fmt"
 	irc "github.com/fluffle/goirc/client"
 	"net/http"
+	"net/url"
 	"strconv"
+	"sync"
 )
+
+type SessionList struct {
+	sync.RWMutex
+	Sessions map[string]Session
+}
 
 type Session struct {
 	Uuid string
 	c *irc.Conn
+	data *ThemeData
 }
 
 type chatHandler struct {
+	sessionList *SessionList
 }
+
 type loginHandler struct {
+}
+
+type ThemeData struct {
+	Messages map[string]Message
+}
+
+type Message struct {
+	Text string
 }
 
 func (c *chatHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -23,10 +41,17 @@ func (c *chatHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println(accessMsg)
 
 	r.ParseForm()
-	if true {
+	session := getSession(r.Form, c.sessionList)
+	//TODO: validate if uuid exists
+	if session == nil {
 		http.Redirect(w, r, "login", 302)
+		return
 	}
-	w.Write([]byte("Hello IRC"))
+
+	data := displayChat(session)
+
+	//w.Write([]byte("Hello IRC"))
+	w.Write(data)
 }
 
 func (c *loginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -36,8 +61,8 @@ func (c *loginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello LOGIN"))
 }
 
-func newChatHandler() *chatHandler {
-	c := &chatHandler{}
+func newChatHandler(s *SessionList) *chatHandler {
+	c := &chatHandler{s}
 	return c
 }
 
@@ -46,10 +71,36 @@ func newLoginHandler() *loginHandler {
 	return c
 }
 
+func getSession(values url.Values, sessionList *SessionList) *Session {
+	uuid := values.Get("session")
+	if len(uuid) < 1 {
+		return nil
+	}
+
+	if s, ok := sessionList.Sessions[uuid]; ok {
+		return &s
+	}
+
+	return nil
+}
+
+func displayChat(s *Session) []byte {
+	var output string
+	output = "uuid:" + s.Uuid + "\n **** \n"
+	for m := range s.data.Messages {
+		output = output + "\n---\n" + m
+	}
+
+	return []byte(output)
+}
+
 func main() {
 	log.Println("Starting up server...")
+	sessionList := new(SessionList)
+	sessionList.Sessions = make(map[string]Session)
+
 	mux := http.NewServeMux()
-	mux.Handle("/chat", newChatHandler())
+	mux.Handle("/chat", newChatHandler(sessionList))
 	mux.Handle("/login", newLoginHandler())
 
 	log.Println("Listening...")
